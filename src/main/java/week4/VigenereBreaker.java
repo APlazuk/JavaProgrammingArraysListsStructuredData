0358770e6ea2679e8ca35775889df6d89528f59c
APlazuk <aleksandraplazuk@o2.pl>
2020-11-19 22:33:52 +0100

    week4 - assignment2

--- src/main/java/week4/VigenereBreaker.java
+++ src/main/java/week4/VigenereBreaker.java
@@ -2,7 +2,7 @@ package week4;
 
 import edu.duke.FileResource;
 
-import java.util.ArrayList;
+import java.util.*;
 
 
 public class VigenereBreaker {
@@ -39,13 +39,65 @@ public class VigenereBreaker {
         return keys;
     }
 
-    public void breakVigenere(int keyLength) {
+    public void breakVigenere() {
         FileResource fr = new FileResource();
         String message = fr.asString();
 
-        int[] keys = tryKeyLength(message, keyLength, 'e');
+        FileResource fileResource = new FileResource();
+        HashSet<String> dictionary = readDictionary(fileResource);
 
-        VigenereCipher vc = new VigenereCipher(keys);
-        System.out.println("Decrypted message: " + vc.decrypt(message));
+        System.out.println("Decrypted message: " + breakForLanguage(message, dictionary));
+    }
+
+    public HashSet<String> readDictionary(FileResource fr) {
+        HashSet<String> dictionary = new HashSet<>();
+
+        for (String line : fr.lines()) {
+            String word = line.toLowerCase();
+            dictionary.add(word);
+        }
+        return dictionary;
+    }
+
+    public int countWords(String message, HashSet<String> dictionary) {
+        int count = 0;
+
+        for (String word : message.split("\\W")) {
+            if (dictionary.contains(word.toLowerCase())) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public String breakForLanguage(String encrypted, HashSet<String> dictionary) {
+        ArrayList<Integer> max = new ArrayList<>();
+        int keyLength = 0;
+        int value = 0;
+
+        for (int i = 1; i < 100; i++) {
+            int[] keys = tryKeyLength(encrypted, i, 'e');
+            VigenereCipher vc = new VigenereCipher(keys);
+
+            String decrypt = vc.decrypt(encrypted);
+            max.add(countWords(decrypt, dictionary));
+            Integer maxMatch = max.stream().max(Comparator.comparing(Integer::intValue)).orElseThrow(NoSuchElementException::new);
+
+            if (maxMatch > value) {
+                value = maxMatch;
+                keyLength = i;
+            }
+        }
+
+        Integer maxMatch = max.stream().max(Comparator.comparing(Integer::intValue)).orElseThrow(NoSuchElementException::new);
+        System.out.println(maxMatch);
+
+        int[] rightKeys = tryKeyLength(encrypted, keyLength, 'e');
+        VigenereCipher cipher = new VigenereCipher(rightKeys);
+
+        System.out.println(Arrays.toString(rightKeys));
+        System.out.println(keyLength);
+
+        return cipher.decrypt(encrypted);
     }
 }
